c5ac878aa093dec14be5f602ef2993ecdaa787cc
nihal kumar <kumarnihal829@gmail.com>
2026-04-23 14:26:16 +0530

    Reposition HotelSolutions after TrustedBrands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
         <StatsSection />
         <WelcomeIntro />
         <TrustedBrands />
+        <HotelSolutions />
         <OperationalStrategy />
         <YieldIntelligence />
         <GrowthDrivers />
-        <HotelSolutions />
         <ProblemSection />
       </div>
     </main>
