7a6d0a4b8bc22d6ef41202277ed90b0510ace3fc
nihal kumar <kumarnihal829@gmail.com>
2026-04-24 11:59:27 +0530

    feat: add PartnershipStructures section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { TrustedBrands } from "@/components/sections/TrustedBrands";
 import { YieldIntelligence } from "@/components/sections/YieldIntelligence";
 import { GrowthDrivers } from "@/components/sections/GrowthDrivers";
 import { HotelSolutions } from "@/components/sections/HotelSolutions";
+import { PartnershipStructures } from "@/components/sections/PartnershipStructures";
 import { ProblemSection } from "@/components/sections/ProblemSection";
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
         <WelcomeIntro />
         <TrustedBrands />
         <HotelSolutions />
+        <PartnershipStructures />
         <YieldIntelligence />
         <GrowthDrivers />
         <ProblemSection />
